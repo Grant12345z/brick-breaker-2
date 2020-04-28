8d4f3aed57a5a635f108b926cfe99cef879f96c2
Grant12345z <62903954+Grant12345z@users.noreply.github.com>
2020-04-28 14:22:50 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -41,10 +41,10 @@ sprites.onOverlap(SpriteKind.ball, SpriteKind.superBrick, function (sprite, othe
 })
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Projectile, function (sprite, otherSprite) {
     if (otherSprite == superProjectile) {
+        otherSprite.destroy()
         _super = 1
         pause(5000)
         _super = 0
-        otherSprite.destroy()
     } else if (otherSprite == hpProjectile) {
         if (info.life() != 3) {
             info.changeLifeBy(1)
